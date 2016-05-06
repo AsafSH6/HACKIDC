ff796a74a7d107e9ac1cf58c10faf71a908a7312
asaf <asafsemail@gmail.com>
2016-05-06 19:18:35 +0300

    deploy

--- HACKIDC/settings.py
+++ HACKIDC/settings.py
@@ -124,10 +124,10 @@ DATABASES['default'].update(db_from_env)
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_ROOT = 'staticfiles/'
-STATIC_URL = 'static/'
+# STATIC_ROOT = 'staticfiles/'
+STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
