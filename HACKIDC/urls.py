6edb1c36cd285072557599635577dd0929bbf5b4
asaf <asafsemail@gmail.com>
2016-05-06 21:06:57 +0300

    deploy

--- HACKIDC/urls.py
+++ HACKIDC/urls.py
@@ -17,6 +17,9 @@ from django.conf.urls import url, include
 from django.contrib import admin
 import gymmy_app.urls
 
+admin.autodiscover()
+
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/v1/gymmy/', include(gymmy_app.urls)),
--- gymmy_app/admin.py
+++ gymmy_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from gymmy_app.models import *
 
-# Register your models here.
+admin.register(Trainee)
+admin.register(Gym)
+admin.register(GymMachine)
+admin.register(Exercise)
+admin.register(TrainingPlan)
+admin.register(TrainingPlanExerciseDetail)
+admin.register(TrainingPlanExerciseProgress)
+admin.register(PersonalProgress)
