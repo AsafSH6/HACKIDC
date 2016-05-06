1f4202515738442c076142f62c54186e6ddbfffb
asaf <asafsemail@gmail.com>
2016-05-06 20:25:57 +0300

    deploy

--- HACKIDC/settings.py
+++ HACKIDC/settings.py
@@ -128,6 +128,6 @@ STATIC_ROOT = 'staticfiles/'
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
