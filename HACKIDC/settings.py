717ad5a8dd6242a1a362bc2f821a27f7bd43470b
asaf <asafsemail@gmail.com>
2016-05-06 19:17:30 +0300

    deploy

--- HACKIDC/settings.py
+++ HACKIDC/settings.py
@@ -125,7 +125,7 @@ DATABASES['default'].update(db_from_env)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_ROOT = 'staticfiles/'
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
